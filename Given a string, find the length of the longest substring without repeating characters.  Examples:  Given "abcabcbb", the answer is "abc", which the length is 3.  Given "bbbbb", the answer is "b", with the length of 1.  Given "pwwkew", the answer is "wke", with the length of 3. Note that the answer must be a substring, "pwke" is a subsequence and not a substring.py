e1ba4714a05fd770acd0e83e3df147f94bbcb1f2
hxypqr <30620900+hxypqr@users.noreply.github.com>
2017-08-04 18:45:16 +0800

    Update Given a string, find the length of the longest substring without repeating characters.  Examples:  Given "abcabcbb", the answer is "abc", which the length is 3.  Given "bbbbb", the answer is "b", with the length of 1.  Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.py

--- Given a string, find the length of the longest substring without repeating characters.  Examples:  Given "abcabcbb", the answer is "abc", which the length is 3.  Given "bbbbb", the answer is "b", with the length of 1.  Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.py
+++ Given a string, find the length of the longest substring without repeating characters.  Examples:  Given "abcabcbb", the answer is "abc", which the length is 3.  Given "bbbbb", the answer is "b", with the length of 1.  Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.py
@@ -1,1 +1,12 @@
-
+a=["a","s","g","h","g","g","r","b","a"]
+b=a
+print len(a)
+print b
+k=0
+r=0
+t=0
+l=0
+for k in range(0,len(a)):
+    for r in range(k+1,len(a)):
+        for t in range(k,r-1):
+            if a[t]==a[r]:
